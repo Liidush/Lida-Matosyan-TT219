9886fb61bc5a4d64579a0149c6610c1769c8d6e6
Lidush1569 <lidmm54@gmail.com>
2024-10-20 15:53:46 +0400

    Add files via upload

---
+++ Missed_Number.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class MissingNumberFinder {
+public:
+    MissingNumberFinder(const vector<int>& nums, int n) : numbers(nums), maxNumber(n) {}
+
+    int findMissingNumber() {
+
+        int expectedSum = (maxNumber * (maxNumber + 1)) / 2;
+
+     
+        int actualSum = 0;
+        for (int num : numbers) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+
+private:
+    vector<int> numbers;
+    int maxNumber;
+};
+
+int main() {
+    vector<int> numbers = { 1, 2, 3, 5 }; 
+    int n = 5; 
+
+    MissingNumberFinder finder(numbers, n);
+    int missingNumber = finder.findMissingNumber();
+    cout << "Missing Number: " << missingNumber << endl;
+
+    return 0;
+}
